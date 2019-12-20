872266ef2631bfdeffff57f60a633b8308d88c9b
BeratCEM <44165072+BeratCEM@users.noreply.github.com>
2019-12-20 14:23:28 +0300

    movie api movies directorleriyle birlikte geliyor.

--- routes/movie.js
+++ routes/movie.js
@@ -32,7 +32,19 @@ promise.then((data) => {
 });
 
   router.get('/',(req,res) =>{ 
-      const promise = Movie.find({ });
+      const promise = Movie.aggregate([
+        {
+          $lookup:{
+            from:'directors',
+            localField: 'director_id',
+            foreignField: '_id',
+            as: 'director'
+          }
+        },
+        {
+          $undwind:'$director'
+        }
+      ]);
       promise.then((data) =>{
         res.json(data);
       }).catch((err) =>{
